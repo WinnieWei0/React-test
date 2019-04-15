ca15c38f03703b02e349a57b4575fb5357ebb706
WinnieWei0 <1344228500@qq.com>
2019-04-15 18:05:41 +0800

    first commit

--- src/Focus.js
+++ src/Focus.js
@@ -1,58 +1,53 @@
-import React, { Component } from 'react';
-// class Focus extends Component{
-//   constructor(){
-//     return {
-//       userInput:''
-//     }
+import React, { Component }  from 'react';
+// class Focus extends Component {
+//   constructor() {
+//     return { userInput: '' };
 //   }
-//   handleChange(e){
-//     this.setState({userInput:e.target.value})
+
+//   handleChange(e) {
+//     // this.setState({ userInput: e.target.value });
 //   }
-//   clearFocus(){
-//     this.setState({userInput:''},()=>{
-//       this.refs.theInput.focus()
-//     })
+
+//   clearAndFocusInput() {
+//     // this.setState({ userInput: '' }, () => {
+//     //   this.refs.theInput.focus();
+//     // });
 //   }
 
 //   render() {
 //     return (
 //       <div>
-//         <div onClick={this.clearFocus.bind(this)}>
-//         click to focus
+//         <div>
+//         {/* <div onClick={this.clearAndFocusInput.bind(this)}> */}
+//           Click to Focus and Reset
 //         </div>
-//         <input ref="theInput" value={this.state.userInput} onChange={this.handleChange.bind(this)} />
+//         <input
+//           ref="theInput"
+//           value={this.state.userInput}
+//           // onChange={this.handleChange.bind(this)}
+//         />
 //       </div>
-//     )
+//     );
 //   }
 // }
-class Focus extends Component {
+const Focus=()=>{
   constructor() {
-    return { userInput: '' };
+        return { userInput: '' };
+      }
+    
+      handleChange(e) {
+        // this.setState({ userInput: e.target.value });
+      }
+    
+      clearAndFocusInput() {
+        // this.setState({ userInput: '' }, () => {
+        //   this.refs.theInput.focus();
+        // });
+      }
+  return (
+    <div>
+      Click to Focus and Reset
+    </div>
+    )
   }
-
-  handleChange(e) {
-    this.setState({ userInput: e.target.value });
-  }
-
-  clearAndFocusInput() {
-    this.setState({ userInput: '' }, () => {
-      this.refs.theInput.focus();
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <div onClick={this.clearAndFocusInput.bind(this)}>
-          Click to Focus and Reset
-        </div>
-        <input
-          ref="theInput"
-          value={this.state.userInput}
-          onChange={this.handleChange.bind(this)}
-        />
-      </div>
-    );
-  }
-}
 export default Focus
--- src/index.js
+++ src/index.js
@@ -22,3 +22,10 @@ setInterval(tick,1000)
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+
+
+// PS：疑惑
+// 1、“合成事件”和“原生事件”---》什么是合成？什么是原生？
+// 2、findDOMNode() 不能用在无状态组件上---》什么是无状态组件。
